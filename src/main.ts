a9aa4527a8b5fc0796e6b187ffb18cdeed64bc8b
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-07-11 17:00:26 +0900

    historyモードでvuerouterの導入

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import router from './router'
 import Sample from './Sample.vue'
 
 Vue.config.productionTip = false
 
 import VueCompositionApi from "@vue/composition-api";
+
 Vue.use(VueCompositionApi);
 
 new Vue({
-  render: h => h(Sample),
+  router,
+  render: h => h(Sample)
 }).$mount('#app')
