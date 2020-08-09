56505295c80a2159f6b7f48cc89cf9849060c67b
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-09 23:00:31 +0900

    Navbarにコンポーネント化しデザイン変更

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
     <div id="app">
-        <nav>
+        <!-- <nav>
             <router-link to="/">Home</router-link>
             <router-link to="/about">About</router-link>
             <router-link to="/works">Works</router-link>
-        </nav>
+        </nav> -->
+        <Navbar/>
         <router-view/>
     </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from "./components/Navbar.vue";
+export default Vue.extend({
+    name:'App',
+    components:{
+        Navbar
+    }
+})
+</script>
 <style lang="scss">
 
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+    <nav
+    class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <span class="navbar-brand px-5">Hello World!</span>
+        <button
+        class="navbar-toggler" 
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item mx-2">
+                <a
+                class="nav-link active"
+                aria-current="page"
+                href="/"
+                >
+                <span class="d-lg-none font-weight-bold"> > </span>
+                <span v-text="linksText.link1"/>
+                </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link"
+                href="/about">
+                <span class="d-lg-none font-weight-bold"> > </span>
+                <span v-text="linksText.link2"/>
+                </a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link"
+                href="/works">
+                <span class="d-lg-none font-weight-bold"> > </span>
+                <span v-text="linksText.link3"/>
+                </a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+export default defineComponent({
+    setup(){
+        const linksText = ref({
+            link1:"Home",
+            link2:"About",
+            link3:"Works"
+        });
+        return {
+            linksText
+        }
+    }
+})
+</script>
