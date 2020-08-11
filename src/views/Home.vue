f4e9a10e9d0a163731124eb06a3e8d41005d25af
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-11 22:08:19 +0900

    bootstrap5playからの移植

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <home-header/>
+        <div class="text-center">
+            <a
+            href="#h1"
+            class="text-decoration-none">
+                <button
+                type="button"
+                class="btn btn-dark btn-lg px-5"
+                >
+                    ことづについて
+                </button>
+            </a>
+        </div>
+        <div
+        id="icons"
+        class="text-center container d-flex w-50"
+        >
+            <div class="row g-5 text-center my-auto">
+                <icon-sns-link
+                aHref="https://github.com/kotodu"
+                imgName="/github.svg"
+                pText="GitHub : @kotodu"
+                />
+                <icon-sns-link
+                aHref="https://github.com/kotodu"
+                imgName="/Twitter_Social_Icon_Rounded_Square_Color.svg"
+                pText="Twitter : @kotodu"
+                />
+                <icon-sns-link
+                aHref="https://github.com/kotodu"
+                imgName="/Twitter_Social_Icon_Rounded_Square_Color.svg"
+                pText="Twitter : @kotodu"
+                />
+                <icon-sns-link
+                aHref="https://github.com/kotodu"
+                imgName="/Twitter_Social_Icon_Rounded_Square_Color.svg"
+                pText="Twitter : @kotodu"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+<script lang="ts">
+import HomeHeader from '@/components/HomeHeader.vue'
+import IconSnsLink from '@/components/IconSnsLink.vue'
+import Vue from 'vue'
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+import { component } from 'vue/types/umd';
+export default defineComponent({
+    setup(){
+        return {
+        }
+    },
+    components:{
+        HomeHeader,
+        IconSnsLink
+    }
+})
 </script>
+<style lang="scss" scoped>
+</style>
