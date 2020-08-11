6ba9eb435eeb546da21c20f4b69314028358c8d0
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-11 22:08:32 +0900

    Navbarについてのコメント整理

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
     <div id="app">
-        <!-- <nav>
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/works">Works</router-link>
-        </nav> -->
         <Navbar/>
         <router-view/>
     </div>
