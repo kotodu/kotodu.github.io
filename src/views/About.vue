171bffb50eec27e86e83a39d732e1849d88833e5
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-12 22:19:31 +0900

    雑だがcompositionAPIをAboutに仮設

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+import { component } from 'vue/types/umd';
+export default defineComponent({
+    setup(){
+        return {
+        }
+    },
+    components:{
+    }
+})
+</script>
+<style lang="scss" scoped>
+</style>
