f4d3d598159f90ff5ae0597c563006de2aa7b869
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-13 22:19:29 +0900

    Aboutページのコンテンツ実装。スキル概要を示すDigestBoxの新規作成

---
+++ src/components/DigestBox.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="card w-lg-75 container my-3">
+        <div class="row">
+            <div class="col-lg-4 text-center">
+                <img
+                :src="imgSrc"
+                class="w-50 my-5"
+                >
+            </div>
+            <div class="col-lg-8">
+                <div class="card-body">
+                    <h3
+                    v-text="title"/>
+                    <p
+                    class="card-text"
+                    v-text="description"
+                    />
+                    <ul class="list-group list-group-flush">
+                        <li
+                        class="list-group-item"
+                        v-for="object in lists"
+                        :key="object"
+                        v-text="object"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+import { component } from 'vue/types/umd';
+type Props = {
+    lists: Array<String>,
+    imgSrc:String,
+    title:String,
+    description : String
+};
+export default defineComponent({
+    props:{
+        lists:{
+            type:Array
+        },
+        imgSrc:{
+            type:String
+        },
+        title:{
+            type:String
+        },
+        description:{
+            type:String
+        }
+    },
+    setup(props:Props){
+        const lists = props.lists;
+        const imgSrc = props.imgSrc;
+        const title = props.title;
+        const description = props.description;
+        return {
+            lists,
+            imgSrc,
+            title,
+            description
+        }
+    },
+    components:{
+    }
+})
+</script>
+<style lang="scss" scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <div class="w-50 mx-auto">
+            <digest-box
+            title="全般"
+            description="得意とするスキル全般"
+            imgSrc="/img/qiita.svg"
+            :lists="skills"
+            />
+            <digest-box
+            title="資格"
+            description="これまでに取得した資格"
+            imgSrc="/img/qiita.svg"
+            :lists="certifications"
+            />
+            <digest-box
+            title="言語"
+            description="主に得意とする言語。この他にも経験あり"
+            imgSrc="/img/qiita.svg"
+            :lists="langs"
+            />
+            <digest-box
+            title="フレームワーク"
+            description="主に使用しているフレームワーク"
+            imgSrc="/img/qiita.svg"
+            :lists="fws"
+            />
+            <digest-box
+            title="ツール・環境"
+            description="主に使用しているツールや環境"
+            imgSrc="/img/qiita.svg"
+            :lists="envs"
+            />
+            <digest-box
+            title="データベース"
+            description="主に使用しているデータベース"
+            imgSrc="/img/qiita.svg"
+            :lists="dbs"
+            />
+            <digest-box
+            title="地図・地理情報"
+            description="主に触れてきたGISシステムや地図など"
+            imgSrc="/img/qiita.svg"
+            :lists="gis"
+            />
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
 import { component } from 'vue/types/umd';
+
+import DigestBox from '@/components/DigestBox.vue'
+
 export default defineComponent({
     setup(){
+        const certifications = [
+            "ITパスポート",
+            "基本情報技術者試験",
+            "Java SE 7/8 Bronze Online"
+        ];
+        const skills = [
+            "Webアプリケーション制作",
+            "Adobe Illustrator"
+        ];
+        const langs = [
+            "JavaScript/TypeScript",
+            "CSS, SASS(SCSS)",
+            "Illustrator Scripts",
+            "VB.NET"
+        ];
+        const fws = [
+            "Vue.js",
+            "Leaflet.js",
+            "Express.js"
+        ];
+        const envs = [
+            "Node.js",
+            "Visual Studio Code",
+            "Visual Studio",
+            "Git / GitHub"
+        ];
+        const dbs = [
+            "MongoDB",
+            "SQLserver"
+        ];
+        const gis = [
+            "OpenStreetMap"
+        ];
         return {
+            certifications,
+            skills,
+            langs,
+            fws,
+            envs,
+            dbs,
+            gis
         }
     },
     components:{
+        DigestBox
     }
 })
 </script>
