d26aeb8c0567645a2811736e8070bc1953d5eebd
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-08-15 18:13:53 +0900

    workcardの仮実装

---
+++ src/components/WorkCard.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="col-lg-3">
+        <div
+        class="card mx-auto"
+        >
+            <img
+            :src="links.img"
+            class="card-img-top"
+            >
+            <div class="card-body">
+                <h3
+                class="card-title"
+                v-text="text.head"/>
+                <p
+                class="card-text"
+                v-text="text.body"/>
+                <ul
+                class="list-group list-group-flush"
+                v-show="items !== undefined"
+                >
+                    <li
+                    class="list-group-item"
+                    v-for="item in items"
+                    :key="item"
+                    v-text="item"
+                    />
+                </ul>
+                <div
+                class="btn-group"
+                v-show="links.demo !==undefined && links.github !==undefined">
+                    <a
+                    :href="links.demo"
+                    class="btn btn-primary">DEMO</a>
+                    <a
+                    :href="links.github"
+                    class="btn btn-dark">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+import { component } from 'vue/types/umd';
+type Props = {
+    lists: Array<String>,
+    imgSrc:String,
+    title:String,
+    description : String,
+    githubURL:String,
+    demoURL:String
+};
+export default defineComponent({
+    props:{
+        lists:{
+            type:Array
+        },
+        imgSrc:{
+            type:String
+        },
+        title:{
+            type:String
+        },
+        description:{
+            type:String
+        },
+        githubURL:{
+            type:String
+        },
+        demoURL:{
+            type:String
+        }
+    },
+    setup(props:Props){
+        const links = ref({
+            img:props.imgSrc,
+            github:props.githubURL,
+            demo:props.demoURL
+        });
+        const text = ref({
+            head:props.title,
+            body:props.description
+        });
+        const items = props.lists;
+        return {
+            links,
+            text,
+            items
+        }
+    }
+})
+</script>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="works">
-    <p>This is an Works page</p>
-  </div>
+    <div class="works">
+        <header class="text-center my-5">
+            <h1>作品</h1>
+        </header>
+        <main class="container">
+            <div class="row g-5">
+                <work-card
+                title="PrivateTextTool"
+                description="私的な置換ツールです。TypeScript+Vue.js"
+                imgSrc="/img/qiita.svg"
+                demoURL="https://kotodu.io/privateTextTool"
+                githubURL="https://github.com/busroutemap/privateTextTool"
+                />
+                <work-card
+                title="PrivateTextTool"
+                description="私的な置換ツールです。TypeScript+Vue.js"
+                imgSrc="/img/qiita.svg"
+                demoURL="https://kotodu.io/privateTextTool"
+                githubURL="https://github.com/busroutemap/privateTextTool"
+                />
+                <work-card
+                title="PrivateTextTool"
+                description="私的な置換ツールです。TypeScript+Vue.js"
+                imgSrc="/img/qiita.svg"
+                demoURL="https://kotodu.io/privateTextTool"
+                githubURL="https://github.com/busroutemap/privateTextTool"
+                />
+                <work-card
+                title="PrivateTextTool"
+                description="私的な置換ツールです。TypeScript+Vue.js"
+                imgSrc="/img/qiita.svg"
+                :lists="l1"
+                />
+                <work-card
+                title="PrivateTextTool"
+                description="私的な置換ツールです。TypeScript+Vue.js"
+                imgSrc="/img/qiita.svg"
+                />
+            </div>
+        </main>
+    </div>
 </template>
+<script lang="ts">
+import Vue from 'vue';
+import { defineComponent, reactive, ref, computed } from "@vue/composition-api";
+import { component } from 'vue/types/umd';
+
+import WorkCard from '@/components/WorkCard.vue'
+
+export default defineComponent({
+    setup(){
+        const l1 = ["a","b"];
+        return {
+            l1
+        }
+    },
+    components:{
+        WorkCard
+    }
+})
+</script>
