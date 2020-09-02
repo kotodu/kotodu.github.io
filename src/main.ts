842b8bae05b63faf90fae67a59102e27cd7cc286
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-09-02 22:01:23 +0900

    #24 の解決。bootstrapのjsの適用を忘れていた

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import router from './router'
-// import Sample from './Sample.vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
 
+// bootstrap導入
+import 'bootstrap';
+
+// compositionAPI
 import VueCompositionApi from "@vue/composition-api";
-
-
 Vue.use(VueCompositionApi);
 
 new Vue({
